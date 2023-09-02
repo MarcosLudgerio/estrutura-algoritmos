14478da83432f545836f1794fb3b4bc8734a9277
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2023-09-02 08:53:56 -0300

    feat: implemented add element

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import utils.Vetor;
 
 public class Main {
     public static void main(String[] args) {
-        Vetor v = new Vetor(5);
+        Vetor v = new Vetor(2);
 
-        v.adicionar(1);
+        v.adiciona(1);
+        v.adiciona(3);
+        v.adiciona(8);
 
     }
 }
--- src/utils/Vetor.java
+++ src/utils/Vetor.java
@@ -2,17 +2,27 @@ package utils;
 
 public class Vetor {
     private int[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade) {
         this.elementos = new int[capacidade];
+        tamanho = 0;
     }
 
-    public void adicionar(int elemento) {
-        for (int i = 0; i < elementos.length; i++) {
-            if (this.elementos[i] == 0) {
-                this.elementos[i] = elemento;
-                break;
-            }
+//    public void adicionar(int elemento) {
+////        for (int i = 0; i < elementos.length; i++) {
+////            if (this.elementos[i] == 0) {
+////                this.elementos[i] = elemento;
+////                break;
+////            }
+////        }
+
+    public boolean adiciona(int elemento) {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
         }
+        return false;
     }
 }
